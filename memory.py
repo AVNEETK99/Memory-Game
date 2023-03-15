1e4f9d99343b8473d41ecd37c914358429e1058a
AVNEETK99 <57584933+AVNEETK99@users.noreply.github.com>
2023-03-15 15:48:21 +0530

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,16 +1,12 @@
-# import modules
+
 from random import *
 from turtle import *
 
-# set the screen
+
 screen = Screen()
 
-#choose background color
 screen.bgcolor("black")
 
-# define the function
-# for creating a square section
-# for the game
 def Square(x, y):
     up()
     goto(x, y)
@@ -22,18 +18,14 @@ def Square(x, y):
         left(90)
     end_fill()
 
-# define function to
-# keep a check of index number
+
 def Numbering(x, y):
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
-# define function
+
 def Coordinates(count):
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
-# define function
-# to make it interactive 
-# user click
 def click(x, y):
     spot = Numbering(x, y)
     mark = state['mark']
@@ -71,9 +63,6 @@ tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
 
-# for shuffling the
-# numbers placed inside 
-# the square tiles
 shuffle(tiles)
 tracer(False)
 onscreenclick(click)
